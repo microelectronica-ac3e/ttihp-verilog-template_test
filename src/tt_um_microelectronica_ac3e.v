776d6edc7e417dce73759fcc094f4aa899c90ef2
Linux_Nelson <microelectronicaac3e@gmail.com>
2026-02-09 15:56:26 -0300

    Project updated 3

--- src/tt_um_microelectronica_ac3e.v
+++ src/tt_um_microelectronica_ac3e.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_microelectronica-ac3e (
+module tt_um_microelectronica_ac3e (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
